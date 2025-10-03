afb6b3992ed965f8e3a5174737cfb56638a61501
nullchamber <131722143+nullchamber@users.noreply.github.com>
2025-10-03 02:07:06 -0500

    fixed base.html javascript buggs

--- problog/blog/views.py
+++ problog/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.conf import settings
 
-from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.models import Post, Comment
+from blog.forms import EmailPostForm, CommentForm
 
 
 def post_list(request):
@@ -35,11 +36,15 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         slug=post
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
+            'comments': comments,
+            'form': form,
         }
     )
 
@@ -80,5 +85,29 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent,
+        }
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment,
         }
     )
