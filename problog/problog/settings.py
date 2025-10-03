dacdff1a5f2fe13fd303aee54ae9223d8727c049
nullchamber <131722143+nullchamber@users.noreply.github.com>
2025-10-03 02:42:06 -0500

    updated dev database server to local Postgres Server

--- problog/problog/settings.py
+++ problog/problog/settings.py
@@ -81,11 +81,12 @@ WSGI_APPLICATION = 'problog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'onefullsend23',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
