444f1e7be5f8063a36625ea322e7af841e244916
nullchamber <131722143+nullchamber@users.noreply.github.com>
2025-10-03 01:38:48 -0500

    post sharing

--- problog/blog/views.py
+++ problog/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.conf import settings
 
 from blog.models import Post
+from blog.forms import EmailPostForm
 
 
 def post_list(request):
@@ -37,5 +40,45 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {
             'post': post,
+        }
+    )
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f'{cd["name"]} recommends you read "{post.title}"'
+            message = f'''
+            Hey! Check out "{post.title}" at {post_url}
+
+            Comments about this from {cd['name']}:
+            "{cd["comments"]}"'''
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                [cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent,
         }
     )
